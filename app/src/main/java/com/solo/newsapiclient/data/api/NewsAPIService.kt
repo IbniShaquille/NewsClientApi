379ebbb574d8b53443e6d45c5725e10fd5c7b520
Ibni Shaquille Syauqi Ibrahim <ibni.shaquille@ui.ac.id>
2024-08-08 23:52:03 +0700

    Feat: fun getTopHeadlines APIService

---
+++ app/src/main/java/com/solo/newsapiclient/data/api/NewsAPIService.kt
@@ -0,0 +1,21 @@
+package com.solo.newsapiclient.data.api
+
+import com.solo.newsapiclient.data.model.APIResponse
+import com.solo.newsapiclient.data.utils.Resource
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsAPIService {
+
+    @GET("top-headlines")
+    suspend fun getTopHeadlines(
+        @Query("country")
+        country: String,
+        @Query("page")
+        page: Int,
+        @Query("apiKey")
+        apiKey: String
+    ) : Resource<APIResponse>
+
+
+}
