eba94a73de8cb510f095b4d69ace4507e44c2632
Ibni Shaquille Syauqi Ibrahim <ibni.shaquille@ui.ac.id>
2024-08-09 23:51:39 +0700

    feat: add NetModule

---
+++ app/src/main/java/com/solo/newsapiclient/presentation/di/NetModule.kt
@@ -0,0 +1,31 @@
+package com.solo.newsapiclient.presentation.di
+
+import com.solo.newsapiclient.BuildConfig
+import com.solo.newsapiclient.data.api.NewsAPIService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetModule {
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BuildConfig.BASE_URL)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideNewsApiService(retrofit: Retrofit): NewsAPIService {
+        return retrofit.create(NewsAPIService::class.java)
+    }
+}
