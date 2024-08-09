f8a048ed4d013e9d49a691c496c3bdeef71d8fa3
Ibni Shaquille Syauqi Ibrahim <ibni.shaquille@ui.ac.id>
2024-08-09 09:23:03 +0700

    feat: implement getTopHeadlines() NewsRepositoryImpl

---
+++ app/src/main/java/com/solo/newsapiclient/data/repository/NewsRepositoryImpl.kt
@@ -0,0 +1,42 @@
+package com.solo.newsapiclient.data.repository
+
+import com.solo.newsapiclient.data.model.APIResponse
+import com.solo.newsapiclient.data.model.Article
+import com.solo.newsapiclient.data.repository.dataSource.NewsRemoteDataSource
+import com.solo.newsapiclient.data.utils.Resource
+import com.solo.newsapiclient.domain.repository.NewsRepository
+import kotlinx.coroutines.flow.Flow
+import retrofit2.Response
+
+class NewsRepositoryImpl(
+    private val newsRemoteDataSource: NewsRemoteDataSource
+) : NewsRepository {
+    override suspend fun getNewsHeadlines(): Resource<APIResponse> {
+        return responseToResource(newsRemoteDataSource.getTopHeadlines())
+    }
+
+    private fun responseToResource(response: Response<APIResponse>): Resource<APIResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let {
+                return Resource.Success(it)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+
+    override suspend fun getSearchedNews(searchQuery: String): Resource<APIResponse> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun saveNews(article: Article) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteNews(article: Article) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getSavedNews(): Flow<List<Article>> {
+        TODO("Not yet implemented")
+    }
+}
