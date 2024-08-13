1736ac5f1bffd41d15ab8e0bd0cf05bdacc2bf71
Ibni Shaquille Syauqi Ibrahim <ibni.shaquille@ui.ac.id>
2024-08-13 17:07:23 +0700

    feat: implement vm and headline news fragment

--- app/src/main/java/com/solo/newsapiclient/MainActivity.kt
+++ app/src/main/java/com/solo/newsapiclient/MainActivity.kt
@@ -5,11 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.solo.newsapiclient.databinding.ActivityMainBinding
+import com.solo.newsapiclient.presentation.viewmodel.NewsViewModel
+import com.solo.newsapiclient.presentation.viewmodel.NewsViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var factory: NewsViewModelFactory
+    lateinit var viewModel: NewsViewModel
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +31,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bnvNews.setupWithNavController(navController)
+
+        viewModel = ViewModelProvider(this, factory)[NewsViewModel::class.java]
     }
 }
